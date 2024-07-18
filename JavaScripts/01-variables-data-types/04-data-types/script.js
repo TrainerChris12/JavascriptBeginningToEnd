dfdfff1cd9582b4fb953838e5a5784251ebc554d
TrainerChris12 <grantchristopher109@gmail.com>
2024-07-18 18:57:14 -0400

    Completed some of the 4th module of variables-data-types but skipped the rest, as I am already familiar with primitive types

---
+++ JavaScripts/01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,22 @@
+//Primitive Data Types
+
+// //String - Sequence of characters
+ const firstName = 'Sara';
+
+let output = firstName;
+
+console.log(output, typeof output);
+// Number - Integers as well as floating point numbers
+const age = 30;
+
+output = age;
+
+console.log(output, typeof output);
+// Boolean - Logical entity - true or false
+// Null - Intentional absence of any object value
+// Undefined - A Variable that has not yet been defined / assigned
+// Symbol - Built-in object whose constructor returns a unique symbol
+// BigInt - Numbers that are greater than the "Number" type can handle
+
+// Reference types or "objects" are a non-primitive value -- the
+// variable is given a reference to that value
