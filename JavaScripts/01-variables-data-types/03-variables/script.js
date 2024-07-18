20fb6cd54a80c79f045313c66b00f32d034e2d16
TrainerChris12 <grantchristopher109@gmail.com>
2024-07-18 18:32:38 -0400

    Finished 3rd module of variables-data-types

--- JavaScripts/01-variables-data-types/03-variables/script.js
+++ JavaScripts/01-variables-data-types/03-variables/script.js
@@ -38,3 +38,31 @@ console.log(score);
 
 const x = 100;
 
+const arr = [1,2,3,4];
+
+arr.push(5);
+
+console.log(arr);
+
+const person = {
+    name: 'Brad'
+};
+
+person.name = 'John'
+
+console.log(person.name);
+
+person.email = 'brad@gmail.com'
+
+console.log(person.email);
+
+console.log(person);
+
+let a, b, c;
+
+const d = 10,
+    e = 20,
+    f = 30;
+
+console.log(d);
+console.log(a);
