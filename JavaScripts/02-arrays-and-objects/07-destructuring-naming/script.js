246bc83579221ade95c3fc3b459485fb457c646b
TrainerChris12 <grantchristopher109@gmail.com>
2024-07-30 13:58:12 -0400

    .\JavaScripts\02-arrays-and-objects\07-destructuring-naming\ completed

---
+++ JavaScripts/02-arrays-and-objects/07-destructuring-naming/script.js
@@ -0,0 +1,32 @@
+const firstName = 'John';
+const lastName= 'Doe';
+const age = 30;
+
+const person = {
+    firstName,
+    lastName,
+    age,
+};
+
+console.log(person.age);
+
+//Destructuring
+
+const todo = {
+    id: 1,
+    title: 'Take out trash',
+    user: {
+        name: 'John'
+    }
+};
+
+const {id: todoId, title, user: {name}} = todo;
+
+console.log(todoId, title, name);
+
+//Destructure Arrays
+const numbers  = [23, 43, 87, 39];
+
+const [first, second, ...rest] = numbers;
+
+console.log(first, second, rest)
