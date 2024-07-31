6a413d363d590ceea31962c1bce9e23ae583f9f5
TrainerChris12 <grantchristopher109@gmail.com>
2024-07-31 17:06:12 -0400

    .\JavaScripts\03-functions-scope\01-function-basics\ completed

---
+++ JavaScripts/03-functions-scope/01-function-basics/script.js
@@ -0,0 +1,17 @@
+function sayHello() {
+    console.log("Hello World!");
+}
+
+sayHello();
+
+function add(a, b) {
+    console.log(a + b);
+}
+
+add(1, 2);
+
+function subtract (a, b) {
+    return a - b;
+}
+
+console.log(subtract(10, 2));
