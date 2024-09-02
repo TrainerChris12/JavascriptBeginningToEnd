325adf0f8a441a112d75a0f57603b26d659128f5
TrainerChris12 <grantchristopher109@gmail.com>
2024-09-02 13:20:07 -0400

    .\JavaScripts\07-events\10-window-events\ completed

--- JavaScripts/07-events/10-window-events/script.js
+++ JavaScripts/07-events/10-window-events/script.js
@@ -1,1 +1,43 @@
 
+// window.onload = function() {
+// document.querySelector('h1').textContent = 'Welcome to the JavaScript!';
+// }
+
+window.addEventListener('load', function() {
+    console.log('Page Loaded!');
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+    console.log('DOMContentLoaded!');
+})
+
+console.log('run me');
+
+
+window.addEventListener('resize', () => {
+document.querySelector('h1').innerText =
+    `Resized to ${window.innerHeight}px x ${window.innerWidth}px`;
+});
+
+window.addEventListener('scroll', () => {
+    console.log(`Scrolled: ${window.scrollX} x ${window.scrollY}`);
+
+    if (window.scrollY > 70 ) {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    }
+})
+
+window.addEventListener('focus', () => {
+    document.querySelectorAll('p').forEach(p => {
+        p.style.color = 'blue';
+    });
+})
+window.addEventListener('blur', () => {
+    document.querySelectorAll('p').forEach(p => {
+        p.style.color = 'black';
+    });
+})
